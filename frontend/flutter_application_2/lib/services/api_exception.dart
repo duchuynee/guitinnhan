a2586c7694f46bf03f11c8a9bc66f10eb4f5c16c
nguyencuong004 <cuongchumahe@gmail.com>
2026-04-24 13:17:10 +0700

    Add constructor to ApiException for statusCode

--- frontend/flutter_application_2/lib/services/api_exception.dart
+++ frontend/flutter_application_2/lib/services/api_exception.dart
@@ -2,6 +2,8 @@ class ApiException implements Exception {
   final String message;
   final int? statusCode;
 
+  const ApiException(this.message, {this.statusCode});
+
   @override
   String toString() => message;
 }
